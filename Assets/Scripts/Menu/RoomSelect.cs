165f76b8681b773ce98df42223ce43c7a19a8a92
michaelmich3 <michaelmich3@hotmail.com>
2018-02-26 00:51:34 -0600

    Menu - Choice Script And Colliders
    Script that enabled and disables part of the menu level depending on where the player moves (credits, settings, levels, start)

---
+++ Assets/Scripts/Menu/RoomSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSelect : MonoBehaviour
+{
+	[SerializeField] private GameObject unloadRoom;
+	[SerializeField] private GameObject loadRoom;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			unloadRoom.SetActive(false);
+			loadRoom.SetActive(true);
+		}
+	}
+}
